cb95cbb9d5610074b2b6a0a7b18a599b8d96a038
Daniel Oliveira <danielsapup3@gmail.com>
2021-08-04 12:22:19 -0300

    Editar registros na API

--- node_api/app.js
+++ node_api/app.js
@@ -1,58 +1,80 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 require("./models/Artigo");
-const Artigo = mongoose.model('artigo');
+const Artigo = mongoose.model("artigo");
 
 const app = express();
 
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost/banco_d', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(()=>{
+mongoose
+  .connect("mongodb://localhost/banco_d", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log("Conexão com o MongoDB realizada com sucesso!");
-}).catch((erro) =>{
+  })
+  .catch((erro) => {
     console.log("Erro: Conexão com o MongoDB não foi realizada!");
-});
+  });
 
 app.get("/", (req, res) => {
-    Artigo .find({}).then((artigo) => {
-        return res.json(artigo);
-    }).catch((erro) =>{
-        return res.status(400).jason({ 
-            erro: true,
-            menssage: "Nenhum artigo encontrado!"
-        })
+  Artigo.find({})
+    .then((artigo) => {
+      return res.json(artigo);
     })
+    .catch((erro) => {
+      return res.status(400).jason({
+        erro: true,
+        menssage: "Nenhum artigo encontrado!"
+      });
+    });
 });
 
 app.get("/artigo/:id", (req, res) => {
-    Artigo.findOne({_id:req.params.id}).then((artigo) => {
-        return res.json(artigo);
-    }).catch((erro) => {
-        return res.status(400).jason({ 
-            erro: true,
-            menssage: "Nenhum artigo encontrado!"
-        })
-    })    
+  Artigo.findOne({ _id: req.params.id })
+    .then((artigo) => {
+      return res.json(artigo);
+    })
+    .catch((erro) => {
+      return res.status(400).jason({
+        erro: true,
+        menssage: "Nenhum artigo encontrado!"
+      });
+    });
 });
 
 app.post("/artigo", (req, res) => {
-    const artigo = Artigo.create(req.body, (err) =>{
-    if(err) return res.status(400).json({
+  const artigo = Artigo.create(req.body, (err) => {
+    if (err)
+      return res.status(400).json({
         error: true,
         message: "Erro: Artigo não foi cadastrado!"
-    })
+      });
 
     return res.status(400).json({
-        error: true,
-        message: "Artigo cadastrado com sucesso!"
-    })
+      error: true,
+      message: "Artigo cadastrado com sucesso!"
+    });
+  });
 });
+
+app.put("/artigo/:id", (req, res) => {
+  const artigo = Artigo.updateOne({ _id: req.params.id }, req.body, (err) => {
+    if (err) return res.status(400).json({
+        error: true,
+        message: "Erro: Artigo não foi editado!"
+      });
+
+    return res.json({
+        error: true,
+        message: "Artigo editado com sucesso!"
+    });
+  });
 });
 
 app.listen(8080, () => {
-    console.log("Servidor iniciado na porta 8080: http://localhost:8080/");
+  console.log("Servidor iniciado na porta 8080: http://localhost:8080/");
 });
